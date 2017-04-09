d75ea2ca437a56e10f1155977b5fdfbd9dad5cd9
Henry Shen <henry.shen@outlook.com>
2017-04-08 21:04:34 -0400

    getting summary from wikipedia

---
+++ searchWikipedia.py
@@ -0,0 +1,29 @@
+import requests
+import json 
+
+WIKI_SUMMARY_URL = "https://en.wikipedia.org/api/rest_v1/page/summary/{}?redirect=false"
+
+manResponse = ""
+
+#@Mansplain.route("/", methods=["POST"])
+def getSummaryFromWiki(query):
+	formattedURL = WIKI_SUMMARY_URL.format(query)
+	wikiRequest = requests.get(formattedURL)
+	httpBody = wikiRequest.json()
+	
+	if (wikiRequest.status_code >= 400):
+		print("Error page not found")
+		return None
+	else:
+		wikiExtract = httpBody["extract"]
+		return wikiExtract
+
+
+def main():
+	query = "mountain"
+	summary = getSummaryFromWiki(query)
+	print(summary)
+
+
+if __name__ == '__main__':
+	main()
