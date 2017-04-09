a3f813d83ebdb6281d8daddc878783ac4262bef1
erinthered <erinbesswilliams@gmail.com>
2017-04-09 05:30:32 -0400

    Integrate Wiki functionality

---
+++ mansplainer.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+from clarifai.rest import ClarifaiApp
+import searchWikipedia as wiki
+
+#Clarifai keys
+#os.environ["CLARIFAI_APP_ID"]="T6ZF_EuGDplxDhLUk2qtkCvEJdn_lT_usf00zAWc"
+#os.environ["CLARIFAI_APP_SECRET"]="clwcHLrrami5VJj2PzIGSc7tiwFUGbTsP8Fg0HDd"
+appC = ClarifaiApp("T6ZF_EuGDplxDhLUk2qtkCvEJdn_lT_usf00zAWc", "clwcHLrrami5VJj2PzIGSc7tiwFUGbTsP8Fg0HDd")  # assumes environment variables are set.
+#model = app.models.get("general-v1.3")
+
+from flask import Flask, request, redirect
+from twilio.rest import Client
+from twilio import twiml
+from twilio.twiml.messaging_response import MessagingResponse
+import requests
+
+account_sid = "ACbe0730baeb75b5f6a870f65bfabe386d"
+auth_token = "0d08cdad7424c231bc1f34a6b3456777"
+client = Client(account_sid, auth_token)
+
+app = Flask(__name__)
+
+@app.route("/incoming_sms", methods=['GET', 'POST'])
+
+def incoming_sms():
+
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+
+    body = request.form['Body']
+    from_number = request.form['From']
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Determine the right reply for this message
+
+    if body == 'Mansplain it to me!':
+
+        #text reply to new participant with welcome and first clue of hunt
+        resp.message("Hi there sweetheart, what would you like mansplained to you today? You can text our man experts a nice photo and they can help you understand what it's about.")
+        return str(resp)
+
+    #elif body == "TEXT":
+
+    #    resp.message("Thanks for the text, sweetheart. Let me go get a man to explain that to you.")
+
+        #CODE TO RETURN WIKI RESULTS
+
+    else:
+
+        myURL = request.form["MediaUrl0"]
+        req = requests.get(myURL)
+        image_URL = req.url
+
+        model = appC.models.get("general-v1.3")
+
+        imageresult = model.predict_by_url(url=image_URL)
+
+        #resp.message("Okay, I can understand how a girl like you might find that confusing. Let me mansplain that for you: {} I hope that was helpful, but you still look a little lost. How about we discuss this further over a nice dinner? Don't worry, I have your number now, so I'll call you.".format())
+
+        tag = imageresult["outputs"][0]["data"]["concepts"][0]["name"]
+        tag2 = imageresult["outputs"][0]["data"]["concepts"][1]["name"]
+
+        mansplanation = wiki.getSummaryFromWiki(tag)
+        if mansplanation = None:
+            mansplain = wiki.getSummaryFromWiki(tag2)
+
+        resp.message("Okay, I can understand how a girl like you might find that confusing. Let me mansplain that for you: \n {} I hope that was helpful, but you still look a little lost. How about we discuss this further over a nice dinner? Don't worry, I have your number now, so I'll call you.".format(str(mansplanation)))
+        #resp.message(str(mansplanation))
+        #return str(resp)
+
+        #resp.message("I hope that was helpful, but you still look a little lost. How about we discuss this further over a nice dinner? Don't worry, I have your number now, so I'll call you.")
+        return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
