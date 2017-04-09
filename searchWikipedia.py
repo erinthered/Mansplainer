df783fd11aba09f4e623828761aa20ebadbfedea
Henry Shen <henry.shen@outlook.com>
2017-04-09 08:43:38 -0400

    Update searchWikipedia.py

--- searchWikipedia.py
+++ searchWikipedia.py
@@ -3,12 +3,9 @@
 # from nltk.tokenize import sent_tokenize, word_tokenize
 
 import requests
-<<<<<<< HEAD
 import json 
 import sys
-=======
-import json
->>>>>>> 72dbba66ffec30f964a3685253f327b63ac0eff9
+
 
 WIKI_SUMMARY_URL = "https://simple.wikipedia.org/api/rest_v1/page/summary/{}?redirect=false"
 
@@ -17,7 +14,6 @@ startSentence = "Here, let me explain it to you."
 
 
 def getSummaryFromWiki(query):
-<<<<<<< HEAD
 	formattedURL = WIKI_SUMMARY_URL.format(query)
 	wikiRequest = requests.get(formattedURL)
 	httpBody = wikiRequest.json()
@@ -30,23 +26,6 @@ def getSummaryFromWiki(query):
 		print(abbrevExtract)			
 		return abbrevExtract
 
-	
-=======
-    formattedURL = WIKI_SUMMARY_URL.format(query)
-    wikiRequest = requests.get(formattedURL)
-    httpBody = wikiRequest.json()
-
-    if (wikiRequest.status_code >= 400):
-        print("Error page not found")
-        return None
-    else:
-        wikiExtract = httpBody["extract"]
-        abbrevExtract = wikiExtract.split(".")[:2]
-        print(abbrevExtract)
-        return abbrevExtract
-
-
->>>>>>> 72dbba66ffec30f964a3685253f327b63ac0eff9
 # def getTokenizedWord(sentence):
 # 	return word_tokenize(sentence)
 
@@ -66,20 +45,11 @@ def getSummaryFromWiki(query):
 # 		leaves.append(subtree.leaves())
 # 	return leaves
 
-
-
-<<<<<<< HEAD
 def main():
 	query = sys.argv[1]
 	summary = getSummaryFromWiki(query)
 	print(summary)
-=======
-#def main():
-#	query = "mountain"
-#	summary = getSummaryFromWiki(query)
-#	print(summary)
->>>>>>> 72dbba66ffec30f964a3685253f327b63ac0eff9
 
 
-#if __name__ == '__main__':
-#	main()
+if __name__ == '__main__':
+	main()
