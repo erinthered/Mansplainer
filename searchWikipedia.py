04d2c0614a2eabb5f236c6a2b1f8dfc086cb6014
Henry Shen <henry.shen@outlook.com>
2017-04-09 00:25:06 -0400

    NLP update!

--- searchWikipedia.py
+++ searchWikipedia.py
@@ -1,11 +1,16 @@
+import nltk 
+from nltk import pos_tag
+from nltk.tokenize import sent_tokenize, word_tokenize
+
 import requests
 import json 
 
-WIKI_SUMMARY_URL = "https://en.wikipedia.org/api/rest_v1/page/summary/{}?redirect=false"
+WIKI_SUMMARY_URL = "https://simple.wikipedia.org/api/rest_v1/page/summary/{}?redirect=false"
 
 manResponse = ""
+startSentence = "Here, let me explain it to you."
 
-#@Mansplain.route("/", methods=["POST"])
+
 def getSummaryFromWiki(query):
 	formattedURL = WIKI_SUMMARY_URL.format(query)
 	wikiRequest = requests.get(formattedURL)
@@ -18,13 +23,37 @@ def getSummaryFromWiki(query):
 		wikiExtract = httpBody["extract"]
 		return wikiExtract
 
-def splitSummary(wikiSummary):
-	return wikiSummary.split(".")
+	
+def getTokenizedWord(sentence):
+	return word_tokenize(sentence)
+
+def getTaggedText(text):
+	tokenizedText = getTokenizedWord(text)
+	tagged = pos_tag(tokenizedText)
+	return tagged
+
+def getNounPhrase(taggedText):
+	grammar = '''NP: {<JJ>*<NNP>*<NN>?}'''
+	grammar2 = '''NP:	{<DT>?<JJ>?<NN>}'''
+	cp = nltk.RegexpParser(grammar2)
+	result = cp.parse(taggedText)
+	leaves = []
+	for subtree in result.subtrees(filter=lambda x: x.label()=="NP"):
+		leaves.append(subtree.leaves())
+	return leaves
+
+
 
 def main():
 	query = "mountain"
 	summary = getSummaryFromWiki(query)
-	print(summary)
+
+	tagSum = getTaggedText(summary)
+	print (tagSum)
+
+	nounPhr = getNounPhrase(tagSum)
+	for leaf in nounPhr:
+		print(leaf)
 
 
 if __name__ == '__main__':
