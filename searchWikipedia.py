3a27c6fc7c5cae9bcbaa7484b182f33531c4b61c
Henry Shen <henry.shen@outlook.com>
2017-04-18 13:13:38 -0400

    debug info for search

--- searchWikipedia.py
+++ searchWikipedia.py
@@ -12,25 +12,19 @@ startSentence = "Here, let me explain it to you."
 
 
 def getSummaryFromWiki(query):
-	print(query)
 	formattedURL = WIKI_SUMMARY_URL.format(query)
-	print(formattedURL)
+	#print(formattedURL)
 	wikiRequest = requests.get(formattedURL)
 	httpBody = wikiRequest.json()
 	pageData = httpBody["query"]["pages"]
 	if ("-1" in pageData):
-		return "Wow that's really complex. I'm sure if I tried to explain this too much blood would go to your brain and not your lady parts" 
+		return "Wow that's really complex. I'm sure if I tried to explain this too much blood would go to your brain and not your lady parts " + query 
 	else:
 		
 		queryResult = pageData.values()
 		resultList = list(queryResult)
 		extract = resultList[0]["extract"]
 		print(type(extract))
-		'''delete this!! dont want to do IO'''
-		resText = open("./res.txt", "wb")
-		resText.write(extract.encode("utf8"))
-		''''''
-		resText.close()
 		return extract
 		# abbrevExtract = extra.split(".")[:3]
 		# extractString = abbrevExtract[0] + "." + abbrevExtract[1] + "." + abbrevExtract[2] + "."
